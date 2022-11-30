43e657d6aa8155fb3e9eee59ffb21f9bfdd9a275
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2022-11-30 18:24:15 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,21 @@
+package com.shiv.BasicJavaPrograms;
+
+import java.util.Scanner;
+
+public class NumberPattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the rows");
+		int rows=sc.nextInt();
+		for(int i=1;i<=rows;i++) {
+			for(int j=i;j<=rows;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		sc.close();
+
+	}
+
+}
