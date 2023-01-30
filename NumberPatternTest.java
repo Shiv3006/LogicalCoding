7f8d90c9ff85ae2311d96b88e3c4445188b7a0b6
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2023-01-30 18:32:27 +0530

    Add files via upload

---
+++ NumberPatternTest.java
@@ -0,0 +1,27 @@
+package com.bnt.starPattern;
+
+public class NumberPatternTest {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+		
+		for (int i =4; i >= 1; i--) {
+			
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
