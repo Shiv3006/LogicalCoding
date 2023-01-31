9465cc067dc1a9821308a6a1c3776088b82bfc4e
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2023-01-31 18:55:51 +0530

    Add files via upload

--- NumberPatternTest.java
+++ NumberPatternTest.java
@@ -6,18 +6,18 @@ public class NumberPatternTest {
 
 		int n = 5;
 		
-		for (int i = 1; i <= n; i++) {
+		for (int i = n; i >= 1; i--) {
 			
-			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
+			for (int j =i; j >=1; j--) {
+				System.out.print(j+ " ");
 			}
 			System.out.println();
 		}
 
 		
-		for (int i =4; i >= 1; i--) {
+		for (int i =2; i <= n; i++) {
 			
-			for (int j = 1; j <= i; j++) {
+			for (int j = 1; j <=i; j++) {
 				System.out.print(j + " ");
 			}
 			System.out.println();
@@ -25,3 +25,6 @@ public class NumberPatternTest {
 	}
 
 }
+
+
+
