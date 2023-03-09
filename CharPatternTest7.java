27ff23fae6ec068261280ae99c37acdf44acaee9
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2023-03-09 18:14:08 +0530

    Add files via upload

---
+++ CharPatternTest7.java
@@ -0,0 +1,23 @@
+package com.bnt.starPattern;
+
+import java.util.Scanner;
+
+public class CharPatternTest7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows");
+		int rows = sc.nextInt();
+		int alpha = 65;
+		for (int i = 1; i <= rows; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print((char) (alpha) + " ");
+				alpha++;
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
